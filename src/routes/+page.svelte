d6e924ed51126764b891a7155bf92621ae4da5f7
selintyrs <soysel01@students.zhaw.ch>
2024-12-14 18:17:41 +0100

    changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to CurlyDB</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<p></p>
+<img src="/images/overview.jpeg" alt="Overview" />
