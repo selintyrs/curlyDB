a7914f3e57335fccbdb872e733990f42b9294090
selintyrs <soysel01@students.zhaw.ch>
2024-12-25 13:50:42 +0100

    test

--- src/routes/hairtypes/+page.svelte
+++ src/routes/hairtypes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
-    export let data; // Die von der `load`-Funktion bereitgestellten Daten
-    const { hairtypes } = data;
-
+  export let data; // Die von der `load`-Funktion bereitgestellten Daten
+  const { hairtypes } = data;
 </script>
 
 {#each hairtypes as hairtype}
+  <a href={"/hairtypes/" + hairtypes._id}>
+    {artist._id}
+  </a>
   <p>{hairtype.hairtype}</p>
 {/each}
-  
