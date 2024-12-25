3e512e55527eb532bb7c69eddb2d33cda4785b6c
selintyrs <soysel01@students.zhaw.ch>
2024-12-25 15:57:40 +0100

    hairtypecard angepasst

--- src/lib/components/HairtypeCard.svelte
+++ src/lib/components/HairtypeCard.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <div class="card text-center mb-3 shadow-sm" style="width: 18rem;">
-    <img src={picture} class="card-img-top" alt={name} style="max-height: 200px; object-fit: cover;" />
+    <img src={hairtypes.picture} class="card-img-top" alt={hairtypes.name} style="max-height: 200px; object-fit: cover;" />
     <div class="card-body">
-      <h5 class="card-title">{name}</h5>
-      <h6 class="card-subtitle text-muted">{type}</h6>
-      <p class="card-text">{description}</p>
+      <h5 class="card-title">{hairtypes.name}</h5>
+      <h6 class="card-subtitle text-muted">{hairtypes.hairtype}</h6>
+      <p class="card-text">{hairtypes.description}</p>
     </div>
   </div>
