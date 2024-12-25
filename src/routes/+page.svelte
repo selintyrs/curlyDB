8e6560f3d6ca976d8dd497a707f46699b97f671f
selintyrs <soysel01@students.zhaw.ch>
2024-12-25 16:00:43 +0100

    added pics and edit start page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<h1>Welcome to CurlyDB</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to your curly hair database</h1>
 
-<img src="/images/overview.jpeg" alt="Overview" />
+
+<img src="/images/models/main.jpeg" alt="main" />
