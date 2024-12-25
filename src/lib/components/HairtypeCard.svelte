b073e4e0f834ecb5b5ab90d364e6b6a7bd8ca62a
selintyrs <soysel01@students.zhaw.ch>
2024-12-25 15:08:18 +0100

    use routinecard

---
+++ src/lib/components/HairtypeCard.svelte
@@ -0,0 +1,12 @@
+<script>
+    let { hairtypes } = $props();
+</script>
+
+<div class="card text-center mb-3 shadow-sm" style="width: 18rem;">
+    <img src={picture} class="card-img-top" alt={name} style="max-height: 200px; object-fit: cover;" />
+    <div class="card-body">
+      <h5 class="card-title">{name}</h5>
+      <h6 class="card-subtitle text-muted">{type}</h6>
+      <p class="card-text">{description}</p>
+    </div>
+  </div>
--- src/routes/hairtypes/[hairtype_id]/+page.svelte
+++ src/routes/hairtypes/[hairtype_id]/+page.svelte
@@ -1,5 +1,15 @@
 <script>
-    let { data } = $props();
+    export let data;
+    const { hairtypes } = data;
+    import RoutineCard from '$lib/components/RoutineCard.svelte';
 </script>
 
 <h1>{data.hairtype._id}</h1>
+
+<div class="row">
+    {#each hairtypes as hairtype}
+    <div class="col-sm-6 col-md-4 col-lg-3 mb-2 gx-2">
+        <RoutineCard hairtype={hairtype} />
+    </div>
+    {/each}
+</div>
