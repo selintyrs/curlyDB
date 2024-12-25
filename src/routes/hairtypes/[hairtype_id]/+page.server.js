377d0ebda22b0fddbfccb6975efd4c47786ab2d1
selintyrs <soysel01@students.zhaw.ch>
2024-12-25 13:57:04 +0100

    test

--- src/routes/hairtypes/[hairtype_id]/+page.server.js
+++ src/routes/hairtypes/[hairtype_id]/+page.server.js
@@ -2,8 +2,6 @@ import db from "$lib/db.js";
 
 export async function load({ params }) {
     return {
-        movie: {
-            _id: params.hairtype_id,
-            ...(await db.getMovie(params.hairtype_id_id)),
-        }    }
-}
+      hairtype: await db.getArtist(params.hairtype_id)
+    };
+  }
