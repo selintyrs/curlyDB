81a20ca5dc5f6b39846b78e0db73f125585d418f
selintyrs <soysel01@students.zhaw.ch>
2024-12-25 15:52:31 +0100

    hairtype card

--- src/routes/hairtypes/+page.svelte
+++ src/routes/hairtypes/+page.svelte
@@ -1,18 +1,19 @@
 <script>
   export let data;
   const { hairtypes } = data;
-  import HairtypeCard from '$lib/components/HairtypeCard.svelte';
+  import HairtypeCard from "$lib/components/HairtypeCard.svelte";
 </script>
 
+<div class="row">
+  {#each hairtypes as hairtype}
+    <div class="col-sm-6 col-md-4 col-lg-3 mb-2 gx-2"></div>
 
-
-{#each data.hairtypes as hairtype}
-      <tr>
-        <td>
-          <a href={"/hairtypes/" + hairtype._id}>
-            {hairtype._id}
-          </a>
-        </td>
-        <td>{hairtype.description}</td>
-        </tr>
-{/each}
+    <a
+      href={`hairtypes/${hairtype._id}`}
+      style="text-decoration: none; color: inherit;"
+    >
+      <HairtypeCard {hairtype} />
+    </a>
+  {/each}
+</div>
+`;
