0d2ebcc37aa232b1ef3b3f6ecbebdec073a5f547
selintyrs <soysel01@students.zhaw.ch>
2024-12-26 17:52:00 +0100

    again

--- src/routes/hairtypes/[hairtype_id]/+page.server.js
+++ src/routes/hairtypes/[hairtype_id]/+page.server.js
@@ -3,11 +3,9 @@ import db from "$lib/db.js";
 export async function load({ params }) {
   const hairtype = await db.getHairType(params.hairtype_id);
   return {
-    props: {
       hairtype: {
         _id: params.hairtype_id,
         ...hairtype,
       }
     }
-  };
-}
+  }
