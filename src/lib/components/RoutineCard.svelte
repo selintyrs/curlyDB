b8240997926c653186147146f1542b3878911d38
selintyrs <soysel01@students.zhaw.ch>
2024-12-26 16:49:29 +0100

    connection

--- src/lib/components/RoutineCard.svelte
+++ src/lib/components/RoutineCard.svelte
@@ -1,10 +1,10 @@
 <script>
-    let { hairtypes } = $props();
+    let { hairtype } = $props();
 </script>
 
 <div class="card-container">
     <div class="card">
-      <img src={hairtypes.routines.washing.image} class="card-img-top" alt="Washing Step" />
+      <img src={hairtype.routines.washing.image} class="card-img-top" alt="Washing Step" />
       <div class="card-body">
         <h5 class="card-title">01 Washing</h5>
         <p class="card-text">Bla bla bla bla bla</p>
@@ -12,7 +12,7 @@
     </div>
   
     <div class="card">
-      <img src={hairtypes.routines.styling.image} class="card-img-top" alt="Styling Step" />
+      <img src={hairtype.routines.styling.image} class="card-img-top" alt="Styling Step" />
       <div class="card-body">
         <h5 class="card-title">02 Styling</h5>
         <p class="card-text">Bla bla bla bla bla</p>
@@ -20,7 +20,7 @@
     </div>
   
     <div class="card">
-      <img src={hairtypes.routines.drying.image} class="card-img-top" alt="Drying Step" />
+      <img src={hairtype.routines.drying.image} class="card-img-top" alt="Drying Step" />
       <div class="card-body">
         <h5 class="card-title">03 Drying</h5>
         <p class="card-text">Bla bla bla bla bla</p>
@@ -28,7 +28,7 @@
     </div>
   
     <div class="card">
-      <img src={hairtypes.routines.refreshing.image} class="card-img-top" alt="Refreshing Step" />
+      <img src={hairtype.routines.refreshing.image} class="card-img-top" alt="Refreshing Step" />
       <div class="card-body">
         <h5 class="card-title">04 Refreshing</h5>
         <p class="card-text">Bla bla bla bla bla</p>
