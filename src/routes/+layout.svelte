f24804c99987c2969902f95164ce6f99a4b14487
selintyrs <soysel01@students.zhaw.ch>
2024-12-28 14:46:25 +0100

    nav collapse

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,15 +16,15 @@
       class="navbar-toggler"
       type="button"
       data-bs-toggle="collapse"
-      data-bs-target="#navbarNav"
-      aria-controls="navbarNav"
+      data-bs-target="navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
       aria-expanded="false"
       aria-label="Toggle navigation"
     >
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/hairtypes" style="color: black;">Hairtypes</a>
         </li>
