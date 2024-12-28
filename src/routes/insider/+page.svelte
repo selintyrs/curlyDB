07dfd85c1362bc576bfa9736661ad0436be48914
selintyrs <soysel01@students.zhaw.ch>
2024-12-28 20:08:56 +0100

    container

--- src/routes/insider/+page.svelte
+++ src/routes/insider/+page.svelte
@@ -13,6 +13,7 @@
     </div>
 </div>
 
+<div class="container">
 <div class="row row-cols-1 row-cols-md-3 g-4">
     {#each insiders as insider}
       <div class="col d-flex align-items-stretch">
@@ -20,6 +21,7 @@
       </div>
     {/each}
   </div>
+</div>
 
 
 <div class="section-2">
