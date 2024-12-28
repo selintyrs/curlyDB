33263c46216429035f064ca8fd4d63109ca90cdc
selintyrs <soysel01@students.zhaw.ch>
2024-12-28 15:21:37 +0100

    nav collapse

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,7 +30,7 @@
       class="navbar-toggler"
       type="button"
       data-bs-toggle="collapse"
-      data-bs-target="navbarSupportedContent"
+      data-bs-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent"
       aria-expanded="false"
       aria-label="Toggle navigation"
