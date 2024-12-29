a4f45b534bb6ed5541fdaacbfe4960f073ad4bc5
selintyrs <soysel01@students.zhaw.ch>
2024-12-29 13:15:32 +0100

    page debugging

--- src/routes/insider/+page.svelte
+++ src/routes/insider/+page.svelte
@@ -2,29 +2,32 @@
     export let data; // Die von der `load`-Funktion bereitgestellten Daten
     const { insiders } = data; // Extrahiere die Filme aus den Daten
     import InsiderCard from "$lib/components/InsiderCard.svelte";
-
 </script>
 
 <div class="section-1">
     <div class="hero-content">
         <h1>Welcome to your <br /> curly hair database</h1>
-        <p>Learn to embrace your curls!</p>¨
-        <a class="btn btn-primary" href={`/insider/create`} role="button">Add New Tip</a>
+        <p>Learn to embrace your curls!</p>
+        ¨
+        <a class="btn btn-primary" href={`/insider/create`} role="button"
+            >Add New Tip</a
+        >
     </div>
 </div>
 
-
 <div class="container">
-<div class="row row-cols-1 row-cols-md-3 g-4">
-    {#each insiders as insider}
-      <div class="col d-flex align-items-stretch">
-          <InsiderCard insider={insider} />
-      </div>
-    {/each}
-  </div>
+    {#if insiders?.length > 0}
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            {#each insiders as insider}
+                <div class="col d-flex align-items-stretch">
+                    <InsiderCard {insider} />
+                </div>
+            {/each}
+        </div>
+    {:else}
+        <p>No insider tips found. Add some tips to get started!</p>
+    {/if}
 </div>
-
-
 
 <div class="section-2">
     <div class="hero-content">
@@ -32,8 +35,7 @@
         <p>Learn to embrace your curls!</p>
     </div>
 </div>
-
-
+;
 
 <style>
     .section-1 {
@@ -55,4 +57,12 @@
         background-repeat: no-repeat;
         position: relative;
     }
-    </style>;
+
+    .container {
+        border: 1px solid red;
+    }
+
+    .row {
+        border: 1px solid blue;
+    }
+</style>
