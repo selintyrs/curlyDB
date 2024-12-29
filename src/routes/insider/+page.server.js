a7db6236e6fb2cb26b5ead2a3db5ab3c01955acc
selintyrs <soysel01@students.zhaw.ch>
2024-12-29 12:58:06 +0100

    load

--- src/routes/insider/+page.server.js
+++ src/routes/insider/+page.server.js
@@ -1,13 +1,5 @@
 import db from "$lib/db.js"
 
-export async function load() {
-
-    return{
-        insiders: await db.createInsider()
-        
-    }
-    
-}
 
 export async function load() {
 
