08b6706a0528bb8900ec290c6a9d1c059309388e
selintyrs <soysel01@students.zhaw.ch>
2024-12-29 13:50:28 +0100

    BACK

--- src/routes/insider/+page.server.js
+++ src/routes/insider/+page.server.js
@@ -2,12 +2,9 @@ import db from "$lib/db.js"
 
 
 export async function load() {
-    console.log("Loaded Insiders Data:", insiders);
-
 
     return{
-        insiders: await db.getInsiders()
-        
+        insiders: await db.createInsider()
         
     }
     
