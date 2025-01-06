9b3de67af58732d320f2fb1d60fc039e9d9ebf84
selintyrs <soysel01@students.zhaw.ch>
2025-01-06 16:01:33 +0100

    serber

--- src/routes/insider/[insider_id]/+page.server.js
+++ src/routes/insider/[insider_id]/+page.server.js
@@ -1,7 +1,7 @@
 import db from "$lib/db.js";
 
 export async function load({ params }) {
-  const hairtype = await db.getInsider(params.insider_id);
+  const insider = await db.getInsider(params.insider_id);
   return {
     insider: {
       _id: params.insider_id,
