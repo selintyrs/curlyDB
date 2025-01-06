6017e38b65e1622bc8aef686e088fc59a1c7fba8
selintyrs <soysel01@students.zhaw.ch>
2025-01-06 18:13:56 +0100

    idk

--- src/routes/insider/[insider_id]/+page.svelte
+++ src/routes/insider/[insider_id]/+page.svelte
@@ -14,16 +14,3 @@
 <h1 class="page-title">{data.insider.tip_for}</h1>
 
 
-<form method="POST" action="?/create">
-    <input type="hidden" name="insiderId" value="{data.insider._id}" />
-    {#each Array(5) as _, i}
-        <input type="radio" name="rating" value="{i + 1}" required />
-    {/each}
-    <button type="submit">Send Rating</button>
-</form>
-
-
-  {#if form?.success}
-  <p>Successfully added!</p>
-{/if}
-  
