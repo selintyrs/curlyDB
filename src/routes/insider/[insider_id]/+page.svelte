846298944f7c82152d5777082fa563331481f71b
selintyrs <soysel01@students.zhaw.ch>
2025-01-06 19:03:53 +0100

    PLS

--- src/routes/insider/[insider_id]/+page.svelte
+++ src/routes/insider/[insider_id]/+page.svelte
@@ -3,24 +3,13 @@
 
     export let data;
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-
-        const formData = new FormData(event.target);
-
-        const response = await fetch(event.target.action, {
-            method: 'POST',
-            body: formData
-        });
-
-        const result = await response.json();
-
-        if (result.success) {
-            window.location.reload(); // Reload to update the page
-        } else {
-            console.error('Failed to save rating:', result);
-        }
-    }
+    function handleSubmit(event) {
+    return async ({ result }) => {
+      if (result.type === "success") {
+        window.location.reload();
+      }
+    };
+  }
 </script>
 
 <h1 class="page-title">{data.insider.tip_for}</h1>
