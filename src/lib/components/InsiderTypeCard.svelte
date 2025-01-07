d54e9ddd6bd898d6e81b1721796c72d8da4f7853
selintyrs <soysel01@students.zhaw.ch>
2025-01-07 03:45:40 +0100

    avg

--- src/lib/components/InsiderTypeCard.svelte
+++ src/lib/components/InsiderTypeCard.svelte
@@ -2,28 +2,27 @@
     export let insider;
 </script>
 
+<ol class="list-group list-group-numbered">
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">{insider.tip_for}</div>
+      {insider.tip_text}
+    </div>
+    <span class="badge text-bg-primary rounded-pill">
+      {insider.ratingAvg ?? '-'}
+    </span>
+  </li>
+</ol>
 
-  <ol class="list-group list-group-numbered">
-    <li class="list-group-item d-flex justify-content-between align-items-start">
-      <div class="ms-2 me-auto">
-        <div class="fw-bold">{insider.tip_for}</div>
-        {insider.tip_text}
-      </div>
-      <span class="badge text-bg-primary rounded-pill">{insider.ratingAvg}</span>
-    </li>
-    </ol>
+<style>
+  .list-group {
+    margin-top: 10px;
+    padding: 10px;
+    background-color: #bd3030;
+  }
 
-  <style>
-    
-    .list-group {
-      margin-top: 10px;
-        padding: 10px;
-        background-color: #bd3030;
-
-    }
-
-    ol li {
+  ol li {
     margin: 10px 0;
     list-style-type: none;
   }
-  </style>
+</style>
